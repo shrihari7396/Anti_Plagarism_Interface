63f4fb4b7404347bdcfd629b2c4c5b0b94d5b73c
SHRIHARI KULKARNI <shriharik121@gmail.com>
2025-07-27 09:23:10 +0530

    Submission Service Started

---
+++ Backend/SubmissionService/src/main/java/edu/pict/sumissionservice/SubmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.pict.sumissionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmissionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmissionServiceApplication.class, args);
+    }
+
+}
