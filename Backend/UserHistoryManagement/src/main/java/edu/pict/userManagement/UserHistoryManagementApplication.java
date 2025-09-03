69dd16f6a3f7dbe29df0cef12209e8228a2c7635
SHRIHARI KULKARNI <shriharik121@gmail.com>
2025-09-03 20:41:36 +0530

    UserManagement:- Implemented Simple crud Operation in project.
    Future Enhancement :-
    Implementation of the user history service for handling this types of stuffs possibly it will relace UserManagement Service/

--- Backend/UserHistoryManagement/src/main/java/edu/pict/userManagement/UserHistoryManagementApplication.java
+++ Backend/UserHistoryManagement/src/main/java/edu/pict/userManagement/UserHistoryManagementApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserHistoryManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserHistoryManagementApplication.class, args);
 	}
-
 }
